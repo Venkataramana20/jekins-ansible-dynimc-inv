385867e04c04ce695f346f5c531192878547d75e
Venkataramana20 <127318500+Venkataramana20@users.noreply.github.com>
2023-05-27 14:43:30 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,16 @@
 provider "aws"{
-region = "eu-west-3"
+region = "ap-south-1"
 }
-
-resource "aws_instance" "AWSServer"{
-ami = "ami-0d767e966f3458eb5"
-instance_type = "t2.micro"
-key_name = "devopsmss"
-security_groups = ["launch-wizard-2"]
-tags={
-Name = "tomcatservers"
-}
+	 
+	 
+resource "aws_instance" ""AWSInstance"{
+	 ami = "ami-0607784b46cbe5816"
+	instane_type = "t2.micro"
+	key_name = "venkataramanachary"
+	security_groups = ["launch-wizard-2"]
+	tags ={
+	Name = "tomcatservers"
+	}
 }
 
 
