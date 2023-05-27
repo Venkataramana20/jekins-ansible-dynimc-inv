adcd4f51bd576c30a91edc7c7e31c31b25e8bfc4
Venkataramana20 <127318500+Venkataramana20@users.noreply.github.com>
2023-05-27 15:46:21 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 	 
 resource "aws_instance" "AWSInstance"{  
    ami = "ami-0607784b46cbe5816"  
-   instane_type = "t2.micro"
+   instance_type = "t2.micro"
    key_name = "venkataramanachary"
    security_groups = ["launch-wizard-2"]
    tags ={
